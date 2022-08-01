80a191ba1fa4be03ef6eb77c8a3b5d66562d3047
Antti Onttonen <antti@neubit.fi>
2022-08-01 13:33:29 +0300

    part_1 exercise_2.10

---
+++ part_1/exercise_2.10/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    // Check if person is already added
+    const personIsAlreadyAdded = persons.find(
+      (person) => person.name === newName
+    );
+    if (personIsAlreadyAdded) {
+      alert(`${newName} is already added to phonebook`);
+      return false;
+    }
+
+    // Create new person object
+    const person = {
+      name: newName,
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(person));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} onChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        onSubmit={addPerson}
+        name={newName}
+        onNameChange={handleNameChange}
+        number={newNumber}
+        onNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+      <Persons
+        persons={persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )}
+      />
+    </div>
+  );
+};
+
+export default App;
