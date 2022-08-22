8f477595f8b7edc089f597320a532913e4a715d3
Antti Onttonen <antti@neubit.fi>
2022-08-22 10:00:05 +0300

    part_3 exercise_3.19

---
+++ part_2/exercise_3.19/src/App.js
@@ -0,0 +1,145 @@
+import { useEffect, useState } from 'react';
+import Filter from './components/Filter';
+import Notification from './components/Notification';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+import personService from './services/persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+  const [notificationMessage, setNotificationMessage] = useState({});
+
+  // Fetch persons data
+  useEffect(() => {
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
+  }, []);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    // Check if person is already added
+    const existingPerson = persons.find((person) => person.name === newName);
+    if (existingPerson) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        updatePerson(existingPerson);
+      }
+      return false;
+    }
+
+    // Add new person to database
+    personService
+      .create({
+        name: newName,
+        number: newNumber,
+      })
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName('');
+        setNewNumber('');
+        handleNotificationMessage({
+          message: `Added ${returnedPerson.name}`,
+          type: 'success',
+        });
+      })
+      .catch((error) => {
+        handleNotificationMessage({
+          message: error.response.data.error,
+          type: 'error',
+        });
+      });
+  };
+
+  const updatePerson = (updatePerson) => {
+    personService
+      .update({ ...updatePerson, number: newNumber })
+      .then((returnedPerson) => {
+        setPersons(
+          persons.map((person) =>
+            person.id !== updatePerson.id ? person : returnedPerson
+          )
+        );
+        handleNotificationMessage({
+          message: `Updated ${returnedPerson.name}`,
+          type: 'success',
+        });
+      })
+      .catch(() => {
+        handleNotificationMessage({
+          message: `Information of ${updatePerson.name} has already been removed from server`,
+          type: 'error',
+        });
+        setPersons(persons.filter((person) => person.id !== updatePerson.id));
+      });
+  };
+
+  const removePerson = (removePerson) => {
+    if (window.confirm(`Delete ${removePerson.name}?`)) {
+      personService.remove(removePerson).then(() => {
+        setPersons(persons.filter((person) => person.id !== removePerson.id));
+        handleNotificationMessage({
+          message: `Deleted ${removePerson.name}`,
+          type: 'success',
+        });
+      });
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleNotificationMessage = ({ message, type }) => {
+    if (message === null) {
+      return false;
+    }
+    setNotificationMessage({ message, type });
+    setTimeout(() => {
+      setNotificationMessage('');
+    }, 5000);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification
+        message={notificationMessage.message}
+        type={notificationMessage.type}
+      />
+      <Filter filter={filter} onChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        onSubmit={addPerson}
+        name={newName}
+        onNameChange={handleNameChange}
+        number={newNumber}
+        onNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+      <Persons
+        persons={persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )}
+        removePerson={removePerson}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ part_2/exercise_3.19/src/services/persons.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const baseUrl = '/api/persons';
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newPerson) => {
+  const request = axios.post(baseUrl, newPerson);
+  return request.then((response) => response.data);
+};
+
+const update = (updatePerson) => {
+  const request = axios.put(`${baseUrl}/${updatePerson.id}`, updatePerson);
+  return request.then((response) => response.data);
+};
+
+const remove = (removePerson) => {
+  return axios.delete(`${baseUrl}/${removePerson.id}`);
+};
+
+const personService = {
+  getAll,
+  create,
+  update,
+  remove,
+};
+
+export default personService;
